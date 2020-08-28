3138ed624fe070e053dba931bf75ca0a0d7bc4ba
Pachauri <prajjwalpachauri725@gmail.com>
2020-08-28 20:22:12 +0530

    Import Activity into SubHunter.java

--- app/src/main/java/com/example/subhunter/SubHunter.java
+++ app/src/main/java/com/example/subhunter/SubHunter.java
@@ -1,10 +1,9 @@
 package com.example.subhunter;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SubHunter extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
