69ec517be5144ba593828101dd3da963045c4ff1
Pachauri <prajjwalpachauri725@gmail.com>
2020-08-28 20:36:24 +0530

    import all necessary statement and methods newGame, onTouchEvent in SubHunter.java class

--- app/src/main/java/com/example/subhunter/SubHunter.java
+++ app/src/main/java/com/example/subhunter/SubHunter.java
@@ -3,16 +3,34 @@ package com.example.subhunter;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.MotionEvent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.view.Display;
+import android.util.Log;
+import android.widget.ImageView;
+import java.util.Random;
 
 public class SubHunter extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("Debugging", "In onCreate");
+        newGame();
+        draw();
         setContentView(R.layout.activity_main);
     }
     @Override
     public boolean onTouchEvent (MotionEvent motionEvent) {
+        Log.d("Debugging", "In onTouchEvent");
+        takeShot();
+
         return true;
+    }
+    public void newGame() {
+        Log.d("Debugging", "In newGame");
     }
 }
