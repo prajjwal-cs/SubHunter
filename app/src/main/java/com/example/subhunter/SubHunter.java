a6866359e066ce95b89ace5599edb76f81ea6097
Pachauri <prajjwalpachauri725@gmail.com>
2020-08-28 20:25:49 +0530

    add onTouchEvent Method in SubHunter.java class

--- app/src/main/java/com/example/subhunter/SubHunter.java
+++ app/src/main/java/com/example/subhunter/SubHunter.java
@@ -2,6 +2,7 @@ package com.example.subhunter;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 
 public class SubHunter extends Activity {
 
@@ -9,5 +10,9 @@ public class SubHunter extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    @Override
+    public boolean onTouchEvent (MotionEvent motionEvent) {
+        return true;
     }
 }
